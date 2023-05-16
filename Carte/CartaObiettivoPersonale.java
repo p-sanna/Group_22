7a4fb257eae16a169363a537c52ac784dcbebde1
frapezzotta <f.pezzotta10@studenti.unibg.it>
2023-05-16 11:56:57 +0200

    completa classe CartaObiettivoComune, che estrae casualmente 2 valori. (da sistemare perchè potrebbe dare  2 frasi uguali. E anche classe CartaObiettivoPersonale

--- Carte/CartaObiettivoPersonale.java
+++ Carte/CartaObiettivoPersonale.java
@@ -1,7 +1,66 @@
 package Carte;
 
-public class CartaObiettivoPersonale {
-	
-	//getImage()
+import java.util.Random;
 
+public class CartaObiettivoPersonale {
+	static String caso;
+	
+	
+	public static void main(String[] args) {
+		
+		
+        // crea oggetto Random
+        Random random = new Random();
+        // genera numero casuale tra 0 e 3
+        int number = random.nextInt(3);
+        switch(number) {
+	case 0: 
+		System.out.println( "");
+		break;
+	case 1:
+		System.out.println ("");
+		break;
+		
+	case 2:
+		System.out.println ("");
+		break;
+		
+	case 3:
+		System.out.println ("");
+		break;
+	case 4:
+		System.out.println ("");
+		break;
+	case 5:
+		System.out.println ("");
+		break;
+	case 6:
+		System.out.println ("");
+		break;
+	case 7:
+		System.out.println ("");
+		break;
+	case 8:
+		System.out.println ("");
+		break;
+	case 9:
+		System.out.println ("");
+		break;
+	case 10:
+		System.out.println ("");
+		break;
+	case 11:
+		System.out.println ("");
+		break;
+	case 12:
+		System.out.println ("");
+		break;
+	}
+	}
+	
 }
+
+	
+
+	
+
