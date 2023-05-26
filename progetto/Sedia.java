4a1323a702c2ace53baa5df198be6708c5ca40c8
frapezzotta <f.pezzotta10@studenti.unibg.it>
2023-05-26 13:58:15 +0200

    .

--- progetto/Sedia.java
+++ progetto/Sedia.java
@@ -5,7 +5,11 @@ import java.util.Random;
 public class Sedia {
 	
 	
+<<<<<<< Updated upstream:progetto/Sedia.java
        public Sedia (int posizione)
+=======
+       public Sedia (int punteggio)
+>>>>>>> Stashed changes:Progetto/Sedia.java
 	{
 	
 		switch (posizione)
@@ -15,21 +19,33 @@ public class Sedia {
 		        Random random = new Random();
 		        // genera numero casuale tra 0 e 1
 		        int number = random.nextInt(1);
+<<<<<<< Updated upstream:progetto/Sedia.java
 		        System.out.println("il giocatore che inizia per primo è il" +number+ " e si prosegue in senso orario");
+=======
+		        System.out.println("il giocatore che inizia per primo è il" +number);
+>>>>>>> Stashed changes:Progetto/Sedia.java
 		        
 			case'3':
 				// crea oggetto Random
 		        Random random1 = new Random();
 		        // genera numero casuale tra 0 e 2
 		        int number1 = random1.nextInt(2);
+<<<<<<< Updated upstream:progetto/Sedia.java
 		        System.out.println("il giocatore che inizia per primo è il"+number1+ " e si prosegue in senso orario");
+=======
+		        System.out.println("il giocatore che inizia per primo è il"+number1);
+>>>>>>> Stashed changes:Progetto/Sedia.java
 		        
 			case'4':
 				// crea oggetto Random
 		        Random random2 = new Random();
 		        // genera numero casuale tra 0 e 3
 		        int number2 = random2.nextInt(3);
+<<<<<<< Updated upstream:progetto/Sedia.java
 		        System.out.println("il giocatore che inizia per primo è il"+number2+ " e si prosegue in senso orario");
+=======
+		        System.out.println("il giocatore che inizia per primo è il"+number2);
+>>>>>>> Stashed changes:Progetto/Sedia.java
 		}
 	}
 	}
