79bd9d3e0260c941eba87d26f5c79c0b69edd162
michaelmarzella <m.marzella@studenti.unibg.it>
2023-05-30 16:40:36 +0200

    UPDATE

--- funzionalità/Ciclico.java
+++ funzionalità/Ciclico.java
@@ -22,3 +22,58 @@ public class Ciclico {
 	}
 
 }
+
+/*
+public class GruppiCaratteriUguali {
+
+    private static char[][] matrice = {
+        {'A', 'A', 'B', 'B', 'C'},
+        {'A', 'A', 'C', 'C', 'C'},
+        {'D', 'D', 'D', 'E', 'E'},
+        {'F', 'F', 'F', 'C', 'C'},
+        {'F', 'F', 'D', 'E', 'B'},
+        {'A', 'F', 'C', 'B', 'A'}
+    };
+
+    private static boolean[][] visitato;
+    private static int righe;
+    private static int colonne;
+
+    public static void main(String[] args) {
+        righe = matrice.length;
+        colonne = matrice[0].length;
+        visitato = new boolean[righe][colonne];
+
+        int gruppi = 0;
+        for (int i = 0; i < righe; i++) {
+            for (int j = 0; j < colonne; j++) {
+                if (!visitato[i][j]) {
+                    char carattere = matrice[i][j];
+                    int elementi = visitaRicorsiva(i, j, carattere);
+                    if (elementi > 0) {
+                        gruppi++;
+                        System.out.println("Gruppo " + gruppi + ": " + elementi + " elementi");
+                    }
+                }
+            }
+        }
+        System.out.println("Totale gruppi: " + gruppi);
+    }
+
+    private static int visitaRicorsiva(int i, int j, char carattere) {
+        if (i < 0 || i >= righe || j < 0 || j >= colonne || visitato[i][j] || matrice[i][j] != carattere) {
+            return 0;
+        }
+
+        visitato[i][j] = true;
+
+        int elementi = 1;
+        elementi += visitaRicorsiva(i - 1, j, carattere); // Sopra
+        elementi += visitaRicorsiva(i + 1, j, carattere); // Sotto
+        elementi += visitaRicorsiva(i, j - 1, carattere); // Sinistra
+        elementi += visitaRicorsiva(i, j + 1, carattere); // Destra
+
+        return elementi;
+    }
+}
+*/
