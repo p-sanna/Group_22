445008af74d994d1a38036db07ce695e29f1464b
Francuini99 <f.peracchi3@studenti.unibg.it>
2023-05-31 16:10:37 +0200

    update
    Co-Authored-By: michaelmarzella <127767963+michaelmarzella@users.noreply.github.com>

--- funzionalità/Ciclico.java
+++ funzionalità/Ciclico.java
@@ -58,5 +58,169 @@ public class Ciclico {
 	        }
 	        return elementi;
 	    }
-	}
+	    
+	    
+	    
+	    
+	    public static boolean  Metodo2(char matrice[][],int ele,int ripetizioni) {
+	        righe = 6;
+	        colonne =5;
+	        boolean a=false;
+	        visitato = new boolean[righe][colonne];
+            ArrayList <Integer> g= new ArrayList();
+	        int gruppi = 0;
+	        for (int i = 0; i < righe; i++) {
+	            for (int j = 0; j < colonne; j++) {
+	                if (!visitato[i][j]) {
+	                    char carattere = matrice[i][j];
+	                    int elementi = visitaRicorsiva(i, j, carattere,matrice);
+	                    if (elementi > ele) {
+	                        gruppi++;
+	                        g.add(elementi);
+	                        System.out.println("Gruppo " + gruppi + ": " + elementi + " elementi");
+	                        
+	                        
+	                    }
+	                }
+	            }
+	        }
+	        System.out.println("Totale gruppi: " + gruppi);
+	         if(gruppi==ripetizioni)
+	         {
+	        	 a= true;
+	         }
+			return a;
+	        
+	    }
+	    
+	    public boolean Metodo3(char a[][])
+	    {
+	    	boolean b=false;
+	    	if(a[0][0]==a[0][4]&&a[0][0]==a[5][0]&&a[0][0]==a[5][4])
+	    	{
+	    		b=true;
+	    	}
+	    	return b;
+	    }
+	    
+	    public boolean Metodo4(char a[][]) //metodo 4blocchi
+	    {
+	    	int j=0,contatore=0;
+	    	boolean b=false;
+	    	do
+	    	{
+	    		for(int i=0;i+1<6;i++)
+	    		{
+	    			if(a[i][j]==a[i][j+1]&&a[i][j]==a[i+1][j]&&a[i+1][j+1]==a[i][j])
+	    			{
+	    				contatore++;
+	    			}
+	    		}
+	    		j++;	
+	    	}while(j+1<5);
+	    		
+	    	if(contatore==2)
+	    	{
+	    		b=true;
+	    		
+	    	}
+	    	return b;
+	    }
+	    public boolean Metodo5(char a[][])
+	    {
+	    	int contatore1 = 0,contatore2 = 0,j=0;
+	    	boolean b=false;
+	    	do
+	    	{
+	    		for(int i=0;i<5;i++)
+	    		{
+	    			if(a[i][j]!=0)
+	    			{
+	    				if(a[i+1][j]!=a[i][j])
+	    				{
+	    					contatore1++;
+	    				}
+	    			}
+	    			if(contatore1<=3)
+	    			{
+	    				contatore2++;
+	    			}
+	    		}
+	    	j++;}while(j<5);
+	    	
+	    	if(contatore2>=3)
+	    	{
+	    		b=true;
+	    	}
+	    	return b;
+	    }
+	    
+	    public boolean Metodo6(char a[][])
+	    {
+	    	char app=a[0][0];
+	    	int contw=0,contb=0,contg=0,contr=0,contv=0,conta=0;
+	    	boolean b=false;
+	    	   for(int i=0;i<6;i++)
+	    		      for(int j=0;j<5;j++)
+	    		      {
+	    		    	  if (a[i][j]!=0&&a[i][j]=='W')
+	    		    	  {
+	    		    		  contw++;
+	    		    	  }
+	    		    	  if (a[i][j]!=0&&a[i][j]=='B')
+	    		    	  {
+	    		    		  contb++;
+	    		    	  }
+	    		    	  if (a[i][j]!=0&&a[i][j]=='G')
+	    		    	  {
+	    		    		  contg++;
+	    		    	  }
+	    		    	  if (a[i][j]!=0&&a[i][j]=='R')
+	    		    	  {
+	    		    		  contr++;
+	    		    	  }
+	    		    	  if (a[i][j]!=0&&a[i][j]=='V')
+	    		    	  {
+	    		    		  contv++;
+	    		    	  }
+	    		    	  
+	    		    	  if (a[i][j]!=0&&a[i][j]=='A')
+	    		    	  {
+	    		    		  conta++;
+	    		    	  }
+	    		    	  
+	    		      }
+	    
+	     if(contv>=8||conta>=8||contw>=8||contr>=8||contg>=8||contb>=8)
+	     {
+	    	 b=true;
+	     }
+	     return b;
+	    }  
+	    
+	  public boolean Metodo7(char a[][])  
+	  {
+		  boolean b=false;
+		     //prima diagonale
+		  
+		       if(a[0][0]==a[1][1]&&a[0][0]==a[2][2]&&a[0][0]==a[3][3]&&a[0][0]==a[4][4])
+		       {
+		    	   b=true;
+		       }
+		       if(a[1][0]==a[2][1]&&a[1][0]==a[3][2]&&a[1][0]==a[4][3]&&a[1][0]==a[5][4])
+		       {
+		    	   b=true;
+		       }
+		       if(a[0][4]==a[1][3]&&a[0][4]==a[2][2]&&a[0][4]==a[3][1]&&a[0][4]==a[4][0])
+		       {
+		    	   b=true;
+		       }
+		       if(a[1][4]==a[2][3]&&a[1][4]==a[3][2]&&a[1][4]==a[4][1]&&a[1][4]==a[5][0])
+		       {
+		    	   b=true;
+		       }
+		       return b;
+	  }
+	    
+}
 
